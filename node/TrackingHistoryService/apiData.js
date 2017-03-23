f3f2276c22761b62d55febba85b9f61f42eb21c0
AvikS <avik.sengupta@royalmail.com>
2017-03-23 11:49:39 +0000

    two files added for data retireval and summary

---
+++ node/TrackingHistoryService/apiData.js
@@ -0,0 +1,8 @@
+var request = require('request');
+var config = require('./config.json');
+var f = module.exports = {};
+
+f.getDataFromIBM = function (param1, param2) { // replace params here
+	// code goes here
+	//return
+};
